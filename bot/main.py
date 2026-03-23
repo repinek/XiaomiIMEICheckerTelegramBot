90f870060add73fcb39310c60bc453ca9c95e84e
repinek <137826826+repinek@users.noreply.github.com>
2026-03-23 16:01:44 +0500

    fix: add drop pending updates

--- bot/main.py
+++ bot/main.py
@@ -183,6 +183,7 @@ async def imei_help(callback: types.CallbackQuery) -> None:
 
 
 async def main() -> None:
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
